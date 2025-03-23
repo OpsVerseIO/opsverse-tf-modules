1870dea4a6dea71606c8880d3bfb4637c5b21759
Arul Jegadish <98673625+arul-opsverse@users.noreply.github.com>
2025-03-22 21:55:53 -0700

    Refactor EC2 module to use data sources, add structured module files for EC2 and S3

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,25 +1,20 @@
-resource "aws_instance" "ec2_instance" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.opsverse_official.id]
-  subnet_id     = aws_subnet.default.id
-}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"]
+data "aws_vpc" "apps_vpc" {
   filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-*-amd64-server-*"]
+    name   = "tag:Name"
+    values = ["apps-vpc"]
   }
 }
 
-resource "aws_security_group" "opsverse_official" {
-  name        = "opsverse-official"
-  description = "OpsVerse official security group"
-  vpc_id      = aws_vpc.apps_vpc.id
+data "aws_security_group" "opsverse_official" {
+  filter {
+    name   = "group-name"
+    values = ["opsverse-official"]
+  }
 }
 
-resource "aws_vpc" "apps_vpc" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_instance" "ec2_instance" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [data.aws_security_group.opsverse_official.id]
+  subnet_id     = data.aws_vpc.apps_vpc.id
 }
